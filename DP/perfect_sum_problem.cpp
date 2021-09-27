0738d9a2d5d640d48132a6bc80985c4d804fbc1a
varun pratap singh <mcs20013@iiitl.ac.in>
2021-09-27 16:45:44 +0530

    Update perfect_sum_problem.cpp

--- DP/perfect_sum_problem.cpp
+++ DP/perfect_sum_problem.cpp
@@ -6,26 +6,61 @@ using namespace std;
 class Solution {
 
 public:
-	int fun(int index, int sum, int target, int arr[], int n, int dp[1000][1000])
-	{
-		if (sum > target) return 0;
-		if (sum == target) return 1;
-		if (index == n) return 0;
-		if (dp[index][sum] != -1) return dp[index][sum];
 
-		int l = fun(index + 1, sum + arr[index], target, arr, n, dp);
-		int r = fun(index + 1, sum, target, arr, n, dp);
+    // memoization code
+    //int dp[1000][1000];
+//  int fun(int index, int sum, int target,int arr[], int n)
+//  {
 
-		return dp[index][sum] = (l + r) % 1000000007;
-	}
-	int perfectSum(int arr[], int n, int sum)
-	{
-		// Your code goes here
-		int dp[1000][1000];
-		memset(dp, -1, sizeof(dp));
+//      if(sum > target) return 0;
+//      if(sum==target) return 1;
+//      if(index==n) return 0;
 
-		return fun(0, 0, sum, arr, n, dp);
-	}
+//      if(dp[index][sum]!=-1) return dp[index][sum];
+
+//      int l = fun(index+1, sum+ arr[index], target,arr, n);
+//      int r= fun(index+1, sum, target, arr, n);
+
+//      return dp[index][sum]= (l+r)%1000000007;
+//  }
+    int perfectSum(int arr[], int n, int sum)
+    {
+        // Your code goes here
+        // memset(dp,-1,sizeof(dp));
+        // return fun(0,0,sum,arr,n);
+
+
+        // tabular approach
+
+        int dp[n + 1][sum + 1];
+
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                if (j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+
+        dp[1][arr[0]] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 1; j <= sum; j++)
+            {
+                if (arr[i - 1] <= j)
+                    dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - arr[i - 1]]) % 1000000007;
+                else
+                    dp[i][j] = dp[i - 1][j];
+
+            }
+        }
+
+        return dp[n][sum];
+    }
 
 };
 
@@ -34,24 +69,24 @@ int main()
 {
 
 
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		int n, sum;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
 
-		cin >> n >> sum;
+        cin >> n >> sum;
 
-		int a[n];
-		for (int i = 0; i < n; i++)
-			cin >> a[i];
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
 
 
-		Solution ob;
-		cout << ob.perfectSum(a, n, sum) << "\n";
+        Solution ob;
+        cout << ob.perfectSum(a, n, sum) << "\n";
 
-	}
-	return 0;
+    }
+    return 0;
 }
 // } Driver Code Ends
