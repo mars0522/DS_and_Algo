25def8f9b1b722a9737bf4965d98257247b8b999
varun pratap singh <mcs20013@iiitl.ac.in>
2021-12-24 19:57:12 +0530

    Update maximum_of_all_subarrays_of_size_k.cpp

--- Sliding_window/maximum_of_all_subarrays_of_size_k.cpp
+++ Sliding_window/maximum_of_all_subarrays_of_size_k.cpp
@@ -1,44 +1,120 @@
-class Solution {
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
 
-        int n = nums.size();
-        stack<int>s;
-        vector<int>nge(n);
-        vector<int>res(n - k + 1);
-        nge[n - 1] = n;
-        s.push(n - 1);
+        // Approach 1 (using next greater element using stack)
 
-        int p = n - 2;
+        // vector<int>res(n-k+1);
+        // vector<int>nge(n);
+        // stack<int>s;
+        // int p=n-2;
 
-        for (int j = n - 2; j >= 0; j--)
+        // s.push(n-1);
+        // nge[n-1]=n;
+
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     while(!s.empty() and arr[i] >= arr[s.top()])
+        //     s.pop();
+
+        //     if(s.empty())
+        //     nge[p--]= n;
+        //     else
+        //     nge[p--]= s.top();
+
+        //     s.push(i);
+        // }
+
+        // p=0;
+        // int j=0;
+
+        // for(int i=0;i<=n-k;i++)
+        // {
+        //     if(j<i)
+        //     j=i;
+
+        //     while(nge[j] < i+k)
+        //     j = nge[j];
+
+        //     res[p++]= arr[j];
+
+        // }
+
+        // return res;
+
+
+        // Approach 2 (using deque)
+
+
+        deque<int>q(k);
+        vector<int>res;
+
+        int i;
+
+        for (i = 0; i < k; i++)
         {
-            while (!s.empty() and nums[j] >= nums[s.top()])
-                s.pop();
+            while (!q.empty() and arr[i] >= arr[q.back()])
+                q.pop_back();
 
-            if (s.empty())
-                nge[p--] = n;
-            else
-                nge[p--] = s.top();
-
-            s.push(j);
+            q.push_back(i);
         }
 
-
-        p = 0;
-
-        int j = 0;
-        for (int i = 0; i <= n - k; i++)
+        for (; i < n; i++)
         {
-            if (j < i)
-                j = i;
+            res.push_back(arr[q.front()]);
 
-            while (nge[j] < i + k)
-                j = nge[j];
+            while (!q.empty() and q.front() <= i - k)
+                q.pop_front();
 
-            res[p++] = nums[j];
+            while (!q.empty() and arr[i] >= arr[q.back()])
+                q.pop_back();
+
+            q.push_back(i);
+
         }
+
+        res.push_back(arr[q.front()]);
 
         return res;
     }
 };
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        Solution ob;
+        vector <int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size (); i++)
+            cout << res[i] << " ";
+        cout << endl;
+
+    }
+
+    return 0;
+}  // } Driver Code Ends
